1c325da7565528faf61d1354bffc79564890f20c
AlaaEldin <alaa12122000@gmail.com>
2023-09-08 01:39:27 -0700

    Add aditional

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,13 +13,20 @@
 int main(void)
 {
 	int num1, num2;
-	for(num1=0; num<=9; ++num)
+	for(num1=0; num1<=9; ++num1)
 	{
-		for(num2=0, num<=9; ++num)
+		for(num2=0; num2<=9; ++num2)
 			{
 				if (num1 < num2)
-					putchar(num1);
-					putchar(num2);
+					{
+						putchar(num1 + '0');
+						putchar(num2 + '0');
+						if (!(num1 == 8 && num2 == 9))
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
 			}
 	}
 	putchar(10);
