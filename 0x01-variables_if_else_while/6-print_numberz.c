56980656fd808c487f70fc2b8e5f078d7cb7d005
AlaaEldin <alaa12122000@gmail.com>
2023-09-08 07:14:30 -0700

    Fix 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main -  program that prints all single digit numbers of base 10 starting from 0
+ * main -  program that prints all single
+ * digit numbers of base 10 starting from 0
  *
  * Description: You are not allowed to use any variable of type char
  *
