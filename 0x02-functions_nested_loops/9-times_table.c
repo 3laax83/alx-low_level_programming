7f11e1009a94c9f3f028e1886a99ca3b74966272
AlaaEldin <alaa12122000@gmail.com>
2023-09-12 06:36:44 -0700

    Add & Fix 9-times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,9 +18,9 @@ void times_table(void)
 	{
 	for (counter2 = 0; counter2 <= 9; ++counter2)
 	{
-	if (counter1*counter2 < 10)
+	if (counter1*counter2 <= 9 && counter1*counter2 != 0)
 	_putchar(' ');
-	else
+	else if (counter1*counter2 > 9)
 	_putchar('0' + counter1*counter2 / 10);
 	_putchar('0' + counter1*counter2 % 10);
 	if (counter2 != 9)
