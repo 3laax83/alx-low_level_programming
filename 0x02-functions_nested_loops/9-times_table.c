8cbadacd96d3588680ddc66e581eb5bc0b2aa1e4
AlaaEldin <alaa12122000@gmail.com>
2023-09-12 06:13:43 -0700

    Add & Fix 9-times_table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * times_table -  prints the 9 times
+ * tables
+ *
+ * Description:  prints the 9 times
+ * table, starting with 0.
+ *
+ * Return: None;
+ */
+void times_table(void)
+{
+	int counter1, counter2;
+
+	for (counter1 = 0; counter1 <= 9; ++counter1)
+	{
+	for (counter2 = 0; counter2 <= 9; ++counter2)
+	{
+	_putchar('0' + counter1*counter2);
+	_putchar(',');
+	}
+	putchar('\n');
+	}
+}
