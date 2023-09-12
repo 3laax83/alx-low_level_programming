dd2580c438c93f641e4791c4da1309d609b7aed8
AlaaEldin <alaa12122000@gmail.com>
2023-09-12 06:04:44 -0700

    Add & Fix 7-print_last_digit.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,28 +1,30 @@
 #include <stdio.h>
-#include "_putchar.c"
-/*
- * Function : jack_bauer
- *
- * Description : prints every minute of
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of
  * the day of Jack Bauer, starting
  * from 00:00 to 23:59.
  *
- * Return : None
+ * Description: prints every minute of
+ * the day of Jack Bauer, starting
+ * from 00:00 to 23:59.
+ *
+ * Return: None
  */
 void jack_bauer(void)
 {
 	int counter1, counter2, counter3, counter4, maxlimit2;
 
-	for (counter1 = 0; counter1 <=2; ++counter1)
+	for (counter1 = 0; counter1 <= 2; ++counter1)
 	{
 	if (counter1 == 2)
 	maxlimit2 = 3;
 	else maxlimit2 = 9;
 	for (counter2 = 0; counter2 <= maxlimit2; ++counter2)
 	{
-	for counter3 = 0; counter3 <=5; ++counter3)
+	for (counter3 = 0; counter3 <= 5; ++counter3)
 	{
-	for (counter4 = 0; counter4 <=9; ++counter4)
+	for (counter4 = 0; counter4 <= 9; ++counter4)
 	{
 	_putchar(counter1);
 	_putchar(counter2);
