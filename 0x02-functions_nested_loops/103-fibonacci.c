98f63a7aaa23ad0d8ab44a2cfe0137a6530b6914
AlaaEldin <alaa12122000@gmail.com>
2023-09-12 08:13:40 -0700

    Add & Fix 103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * fibonacci - prints Fibonacci numbers
+ * @num: number
+ *
+ * Description: prints the first 50
+ * Fibonacci numbers, starting with 1
+ * and 2.
+ *
+ * Return: None.
+ */
+void fibonacci(void)
+{
+	long int num1, sum, num2, placeholder;
+
+	num1 = 1;
+	sum = num1;
+	num2 = 2;
+	while (num2 <= 4000000)
+	{
+	sum += num2;
+	placeholder = num2;
+	num2 = num1 + num2;
+	num1 = placeholder;
+	--num;
+	}
+	printf("%ld\n", sum);
+}
+/**
+ * main - main
+ *
+ * Return: 0.
+ */
+int main(void)
+{
+	fibonacci();
+	return (0);
+}
