d48011f88e3e49c7db795e2fb65c8dd2343a8108
AlaaEldin <alaa12122000@gmail.com>
2023-09-12 07:52:50 -0700

    Add & Fix 102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * fibonacci - prints Fibonacci numbers
+ *
+ * Description: prints the first 50
+ * Fibonacci numbers, starting with 1
+ * and 2.
+ *
+ * Return: None.
+ */
+void fibonacci(int num)
+{
+	int num1, num2, placeholder, counter;
+	counter = 0;
+	num1 = 1;
+	num2 = 2;
+
+	while (counter <= num)
+	{
+	printf("%d", num1 + num2);
+	placeholder = num2;
+	num2 = num1 + num2;
+	num1 = placeholder;
+	}
+}
+/**
+ * main - main
+ *
+ * Return: 0.
+ */
+int main(void)
+{
+	fibonacci(50);
+	return (0);
+}
