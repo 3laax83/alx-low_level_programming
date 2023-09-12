e37170e36f3877931a5b0b8e80e0f6e00a4b0982
AlaaEldin <alaa12122000@gmail.com>
2023-09-12 05:41:08 -0700

    #3

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,15 +5,15 @@
  * is alpha
  * @c: letter
  *
- * Description : checks for
+ * Description: checks for
  * alphabetic character
  *
- * Return : 1 if c is letter, 0 if not
+ * Return: 1 if c is letter, 0 if not
  */
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-	return 1;
+	return (1);
 	else
-	return 0;
+	return (0);
 }
