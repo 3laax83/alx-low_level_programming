5e80bcb57bcaa3a74eee1e8d8d16cc8dc6db7d5e
AlaaEldin <alaa12122000@gmail.com>
2023-09-12 05:16:27 -0700

    #3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <ctype.h>
-#include "_putchar.c"
-/*
- * function: _islower
+#include "main.h"
+/**
+ * _islower - check if the letter
+ * is lower case or not
  *
- * Description - returns 1 if c is lower
+ * Description: returns 1 if c is lower
  * otherwise returns 0;
  *
  * Return : Always 0.
