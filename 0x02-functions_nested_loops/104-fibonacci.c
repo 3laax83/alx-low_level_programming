bff77cef332d60dbad7f3f1d520bb1d946b2ebe9
AlaaEldin <alaa12122000@gmail.com>
2023-09-12 13:06:38 -0700

    Add & Fix 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define LARGEST 10000000000
 /**
  * fibonacci - prints Fibonacci numbers
  * @num: number
  *
- * Description: prints the first 50
+ * Description: prints the first 98
  * Fibonacci numbers, starting with 1
  * and 2.
  *
  * Return: None.
  */
-void fibonacci(int num)
+void fibonacci()
 {
 	unsigned long num1 = 1, num2 = 2, counter, num1h1, num1h2, num2h1, num2h2, half1, half2;
 
