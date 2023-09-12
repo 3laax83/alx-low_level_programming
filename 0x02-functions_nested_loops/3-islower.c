41502ceec45911e33d00baf45d723557a69eeba9
AlaaEldin <alaa12122000@gmail.com>
2023-09-12 05:37:36 -0700

    #3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,16 +4,17 @@
 /**
  * _islower - check if the letter
  * is lower case or not
+ * @c: letter
  *
  * Description: returns 1 if c is lower
  * otherwise returns 0;
  *
- * Return : Always 0.
+ * Return: 1 if lower and 0 if not.
  */
 int _islower(int c)
 {
 	if (islower(c) > 0)
-	return 1;
+	return (1);
 	else
-	return 0;
+	return (0);
 }
