0145085f9baa25846f8a482246e7e28a8622b4c8
AlaaEldin <alaa12122000@gmail.com>
2023-09-14 06:07:31 -0700

    Fix #10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,11 +12,12 @@ void print_triangle(int size)
 {
 	int counter1, counter2;
 
-	for (counter1 = 0; counter1 < size; ++counter1)
+	for (counter1 = size; counter1 > 0; --counter1)
 	{
 		for (counter2 = 0; counter2 < counter1; ++counter2)
 			_putchar(' ');
-		putchar('#');
+		for (counter2 = counter1; counter2 < size; ++counter2)
+			_putchar('#');
 		putchar('\n');
 	}
 }
