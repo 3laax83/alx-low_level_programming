b129475be17dcfb111ee2265f1aca32c0530acec
AlaaEldin <alaa12122000@gmail.com>
2023-09-14 05:25:51 -0700

    Fix #5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,13 +14,11 @@ void more_numbers(void)
 	for (i = 0; i < 10; ++i)
 	{
 		for (j = 0; j < 15; ++j)
-			if (j <= 9)
-				_putchar('0' + j);
-			else
-			{
+		{
+			if (j > 9)
 				_putchar('1');
-				_putchar('0' + j % 10);
-			}
+			_putchar('0' + j % 10);
+		}
 		_putchar('\n');
 	}
 }
