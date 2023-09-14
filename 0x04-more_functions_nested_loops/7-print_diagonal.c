97aa2f823511dfd9ea8da22127d9d0754ce46476
AlaaEldin <alaa12122000@gmail.com>
2023-09-14 05:29:03 -0700

    Fix #7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,11 +12,14 @@ void print_diagonal(int n)
 {
 	int counter1, counter2;
 
+	if (n > 0)
+	{
 	for (counter1 = 0; counter1 < n; ++counter1)
 	{
 		for (counter2 = 0; counter2 < counter1 - 1; ++counter2)
 			_putchar(' ');
 		_putchar('\\');
 		putchar('\n');
+	}
 	}
 }
