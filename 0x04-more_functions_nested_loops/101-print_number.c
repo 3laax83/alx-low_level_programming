6e65d9a02f2b2ed1548b5428bc8afd035e228fcf
AlaaEldin <alaa12122000@gmail.com>
2023-09-14 06:33:25 -0700

    Fix #12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,15 +10,15 @@
  */
 void print_number(int n)
 {
-	char strng[(int)(floor(log10(abs(n))) + 1)];
+	char strng[3];
 	int counter;
 
 	sprintf(strng, "%d", n);
 	if (n < 0)
 		_putchar('-');
-	for (counter = 0; counter < floor(log10(abs(n))) + 1; ++counter)
+	for (counter = 0; counter < sizeof(strng)/sizeof(char); ++counter)
 	{
 		_putchar(strng[counter]);
 	}
-	_putchar('n');
+	_putchar('\n');
 }
