6297deecc2be50b87ecdd1fa357db63f5d2506b9
AlaaEldin <alaa12122000@gmail.com>
2023-09-18 00:16:01 -0700

    Add File #4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - printer function.
+ * @s: pointer to char.
+ *
+ * Description: a function that
+ * prints a string, in reverse.
+ *
+ * Return: None.
+ */
+
+void print_rev(char *s)
+{
+	int counter;
+
+	for (counter = strlen(s); counter >= 0; counter--)
+	{
+		_putchar(s[counter]);
+	}
+	_putchar('\n');
+}
