0a545efad26e648a3ebe7b67a3d35224626f9023
AlaaEldin <alaa12122000@gmail.com>
2023-09-18 07:07:40 -0700

    Add File #8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -18,9 +18,7 @@ void print_array(int *a, int n)
 
 	for (; length < n; ++length)
 	{
-		_putchar(a[length]);
-		_putchar(',');
-		_putchar(' ');
+		printf("%d, ", a[length]);
 	}
-	_putchar('\n');
+	printf("\n");
 }
