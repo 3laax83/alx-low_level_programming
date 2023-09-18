b7aa93edb163f354d91866fb8c9b30296def50db
AlaaEldin <alaa12122000@gmail.com>
2023-09-18 06:41:01 -0700

    Add File #7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -22,7 +22,7 @@ void puts_half(char *str)
 		++length;
 	}
 
-	for (counter = (length / 2) + 1; counter <= length; ++counter)
+	for (counter = (length / 2); counter <= length; ++counter)
 	{
 		_putchar(str[counter]);
 	}
