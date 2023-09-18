71c1299a78ab5bcc461149d91803b7f2adee86fd
AlaaEldin <alaa12122000@gmail.com>
2023-09-18 06:21:41 -0700

    Add File #7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts_half - printer function.
+ * @str: string pointer.
+ *
+ * Description:  a function that prints
+ * half of a string.
+ *
+ * Return: None.
+ */
+
+void puts_half(char *str)
+{
+	int counter; int length = 0;
+	while (length >= 0)
+	{
+		if (str[length] == '\0')
+			break;
+		++length;
+	}
+
+	for (counter = length; counter >= length/2; --counter)
+	{
+		_putchar(str[counter]);
+	}
+	_putchar('\n');
+}
