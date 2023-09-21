100fcb4c049f334c0cb12c6a9fcfee66cd660a22
AlaaEldin <alaa12122000@gmail.com>
2023-09-21 04:12:03 -0700

    Fix file #3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -16,5 +16,5 @@ int _strcmp(char *s1, char *s2)
 	while (*s1 == *s2++)
 		if (*s1++ == 0)
 			return (0);
-	return (*(unsigned char *)s1 - *(unsigned char *)--s2);
+	return (*(unsigned char *)s1 - *(unsigned char *) --s2);
 }
