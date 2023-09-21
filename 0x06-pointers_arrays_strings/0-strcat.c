30cbc813f57ecf4ef2c6832672aba830c4f2ae92
AlaaEldin <alaa12122000@gmail.com>
2023-09-21 02:42:58 -0700

    Add file #0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcat - function to merge
+ * @dest: pointer to destined string
+ * @src: pointer to source string
+ *
+ * Description: This function appends the src
+ * string to the dest string, overwriting the
+ * terminating null byte (\0) at the end of dest,
+ * and then adds a terminating null byte
+ *
+ * Return: a pointer to the resulting string dest.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	while (*dest++);
+	dest--;
+	while ((*dest++ = *src++));
+	return (dest);
+}
