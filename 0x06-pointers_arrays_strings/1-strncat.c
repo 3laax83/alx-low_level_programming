2892e88ba81b9a3e3aa5e72f83b0557d190d27a9
AlaaEldin <alaa12122000@gmail.com>
2023-09-21 03:26:20 -0700

    Add File #1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncat -  a function that
+ * concatenates two strings.
+ * @dest: pointer to destined.
+ * @src: pointer to source.
+ * @n: maximum numbers of src chars.
+ *
+ * Description: The _strncat function is
+ * similar to the _strcat function, except that:
+ * it will use at most n bytes from src and
+ * src does not need to be null-terminated
+ * if it contains n or more bytes
+ *
+ * Return: a pointer to the resulting string dest.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char *save = dest;
+
+	for (; *dest; dest++)
+	;
+	while ((*dest++ = *src++) != '\0' && n > 1)
+		n--;
+	return (save);
+}
