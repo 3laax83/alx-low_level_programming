86ced016d239337737f89f023b3cab744d2b56f0
AlaaEldin <alaa12122000@gmail.com>
2023-09-21 13:20:08 -0700

    Fix File #103

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -36,8 +36,8 @@ void rev_string(char *n)
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int overflow = 0, i = 0, j = 0, digits = 0;
-	int val1 = 0, val2 = 0, temp_tot = 0;
+	int carry = 0, i = 0, j = 0, nums = 0;
+	int val1 = 0, val2 = 0, temp = 0;
 
 	while (*(n1 + i) != '\0')
 	i++;
@@ -47,7 +47,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	j--;
 	if (j >= size_r || i >= size_r)
 		return (0);
-	while (j >= 0 || i >= 0 || overflow == 1)
+	while (j >= 0 || i >= 0 || carry == 1)
 	{
 		if (i < 0)
 			val1 = 0;
@@ -57,21 +57,21 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			val2 = 0;
 		else
 			val2 = *(n2 + j) - '0';
-		temp_tot = val1 + val2 + overflow;
-		if (temp_tot >= 10)
-			overflow = 1;
+		temp = val1 + val2 + carry;
+		if (temp >= 10)
+			carry = 1;
 		else
-			overflow = 0;
-		if (digits >= (size_r - 1))
+			caryy = 0;
+		if (nums >= (size_r - 1))
 			return (0);
-		*(r + digits) = (temp_tot % 10) + '0';
-		digits++;
+		*(r + nums) = (temp % 10) + '0';
+		nums++;
 		j--;
 		i--;
 	}
-	if (digits == size_r)
+	if (nums == size_r)
 		return (0);
-	*(r + digits) = '\0';
+	*(r + nums) = '\0';
 	rev_string(r);
 	return (r);
 }
