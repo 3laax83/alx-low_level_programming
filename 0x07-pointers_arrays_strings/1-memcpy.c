aa46646e0698950a4a9ad7fccb4368f132b2006d
AlaaEldin <alaa12122000@gmail.com>
2023-09-25 06:41:04 -0700

    Add file #1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _memcpy - copier function.
+ * @dest: pointer to destined.
+ * @src: pointer to source memory area.
+ * @n: number of bytes.
+ * Description: a function that copies memory area.
+ * Return: a pointer to dest.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	return ((char *) _mempy (dest, src, n) + n);
+}
