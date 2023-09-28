3c27f37535ecd427a21608d2a4a457cd4739eb48
AlaaEldin <alaa12122000@gmail.com>
2023-09-28 08:33:23 -0700

    Add file #0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - string printer
+ * Description: a function that prints a string.
+ */
+
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	_puts_recursion(*s);
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <ctype.h>
+#include <string.h>
+
+
+extern void _puts_recursion(char *s);
+extern void _print_rev_recursion(char *s);
+extern int _strlen_recursion(char *s);
+extern int factorial(int n);
+extern int _pow_recursion(int x, int y);
+extern int _sqrt_recursion(int n);
+extern int is_prime_number(int n);
+extern int is_palindrome(char *s);
+extern int wildcmp(char *s1, char *s2);
+
+
+#endif
