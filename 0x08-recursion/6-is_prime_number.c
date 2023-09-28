6cd783ec7429189fae8c5afac241447202c60273
AlaaEldin <alaa12122000@gmail.com>
2023-09-28 12:21:24 -0700

    Add file #6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * prime - to find if number is prime
+ * @num1: integer.
+ * @num2: integer.
+ * Description: a function to store value of n.
+ * Return: prime status of n
+*/
+
+int prime(int num1, int num2)
+{
+	if (num1 % num2 == 0)
+		return (0);
+	else if (num2 < num1 / 2)
+		return (1);
+	if (num2 < num1 / 2)
+		return (prime(num1, num2 + 1));
+}
+
+
+/**
+ * is_prime_number- finds if n is prime.
+ * @n: integer.
+ * Description: a function that returns 1 if the input integer
+ * is a prime number, otherwise return 0.
+ * Return: 1 if the input integer is a prime number, otherwise return 0.
+ */
+
+int is_prime_number(int n)
+{
+	return (prime(n, 0));
+}
