4b11266fce60c89b1566cea7a150c93057099662
AlaaEldin <alaa12122000@gmail.com>
2023-09-28 10:36:09 -0700

    Add File #2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - length counter.
+ * @s: pointer to string
+ * Description: a function that returns the length of a string.
+ * Return: length of a string.
+ */
+
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s)
+	{
+		_strlen_recursion(s + 1);
+		i++;
+	}
+	return (i);
+}
