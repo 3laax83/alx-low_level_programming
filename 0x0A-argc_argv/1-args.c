8d56d3e481c1f31ad96595ef1242c617542ac7da
AlaaEldin <alaa12122000@gmail.com>
2023-10-02 03:39:25 -0700

    Add & Fix Files

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: number of arguments.
+ * @argv: pointer to arguments.
+ * Description: a program to print the number of arguments.
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+}
