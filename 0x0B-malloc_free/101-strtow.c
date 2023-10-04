2cc68ec4f1b45dbf97f678854713aa04711a2d3d
AlaaEldin <alaa12122000@gmail.com>
2023-10-04 12:06:53 -0700

    Fix file #100

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -73,7 +73,7 @@ char **strtow(char *str)
 					while (temp < size)
 					{
 						split[j][temp] = str[i - size + temp];
-						temp;
+						temp++;
 					}
 					split[j][temp] = '\0';
 					size = temp = 0;
