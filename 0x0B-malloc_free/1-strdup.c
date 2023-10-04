91dd18e6aef22aa1e23cd67e4426d5d6b9dcdbd1
AlaaEldin <alaa12122000@gmail.com>
2023-10-04 07:27:33 -0700

    Fix File #1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -4,9 +4,9 @@
  * _strdup - a copy container
  * @str: pointer to string.
  * Description: a function that returns a pointer to a newly
- * allocated space in memory, which contains a copy of the 
+ * allocated space in memory, which contains a copy of the
  * string given as a parameter.
- * Return: a pointer to a new string which is a duplicate of the 
+ * Return: a pointer to a new string which is a duplicate of the
  * string str, NULL if str = NULL or failed.
  */
 
@@ -14,10 +14,15 @@ char *_strdup(char *str)
 {
 	size_t len;
 	char *copy;
+	bool statement;
+
+	if (str == NULL)
+		return (NULL);
 
 	len = strlen(str) + 1;
-	if (!(copy = malloc((u_int) len)))
-			return (NULL);
+	statement = (copy = malloc((u_int) len);
+	if (!statement)
+		return (NULL);
 	memcpy(copy, str, len);
 	return (copy);
 }
