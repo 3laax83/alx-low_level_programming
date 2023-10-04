7e63da5944f0e04c70669d0074077323973f188b
AlaaEldin <alaa12122000@gmail.com>
2023-10-04 07:50:08 -0700

    Fix File #2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,15 +14,15 @@ char *str_concat(char *s1, char *s2)
 	char *save = s1;
 	int size1 = 0, size2 = 0;
 
-	if (s1 == 0)
+	if (s1 == NULL)
 		s1 = "";
-	if (s2 == 0)
+	if (s2 == NULL)
 		s2 = "";
 	for (; *s1; ++s1)
 		size1++;
 	for (; *s2; ++s2)
 		size2++;
-	save = malloc (size1 * sizeof(char) + size2 * sizeof(char));
+	save = malloc (1 + size1 * sizeof(char) + size2 * sizeof(char));
 	if (save == 0)
 		return (NULL);
 	while ((*s1++ = *s2++) != '\0')
