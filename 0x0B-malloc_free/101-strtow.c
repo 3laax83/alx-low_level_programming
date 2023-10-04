54d59f0474388708eaa1ebc0251408fdfee4a8a3
AlaaEldin <alaa12122000@gmail.com>
2023-10-04 13:36:29 -0700

    Fix file #100

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -62,7 +62,7 @@ char **strtow(char *str)
 	int total = 0, a = 0, b = 0, length = 0;
 	char **words, *found;
 
-	if (str == NULL || *str == 0 || (*str == 32 && *(str + 1) == '\0'))
+	if (str == NULL || *str == 0 || (*str == 32 && *(str + 1) == 0))
 		return (NULL);
 	total = word_count(str);
 	if (total == 0)
