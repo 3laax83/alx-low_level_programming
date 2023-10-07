c5a4afcb7f96a65b33d9082b2e33f29cf27d5b1f
AlaaEldin <alaa12122000@gmail.com>
2023-10-07 12:41:37 -0700

    Add and/or Fix

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -29,14 +29,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	int newN = (int16_t) n;
 	char *save;
-	int len1 = _strlen(s1);
-	int len2 = _strlen(s2);
-	int i = 0;
+	int len1, len2, i;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
 
 	if (newN >= len2)
 	{
@@ -50,6 +51,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (save == NULL)
 		return (NULL);
+
+	i = 0;
 
 	while ((save[i] = s1[i]))
 		++i;
