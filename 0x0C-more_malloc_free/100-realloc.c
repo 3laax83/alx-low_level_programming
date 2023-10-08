c0979d6bcb81746075c894e727cc1045c98572b0
AlaaEldin <alaa12122000@gmail.com>
2023-10-07 23:38:35 -0700

    Fix file #100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,7 +17,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (ptr);
 
 	if (ptr == NULL)
-		ptr = malloc(old_size);
+	{
+		ptr = malloc(new_size);
+	}
 
 	if (new_size == 0)
 	{
