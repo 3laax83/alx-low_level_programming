b4ba40e1353dca2fbf4beb37c29b49a18b6e213c
AlaaEldin <alaa12122000@gmail.com>
2023-10-07 23:47:01 -0700

    Fix file #100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,15 +13,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	void *newData = NULL;
 	int min;
 
-	if (new_size == old_size || new_size == 0)
-	{
-		free(ptr);
-		return (NULL);
-	}
-
 	if (ptr == NULL)
 		ptr = malloc(old_size);
 
+	if (new_size == old_size)
+		return (ptr);
+	
 	newData = malloc(new_size);
 
 	if (newData == NULL)
