e9315e1dece6c94646abd2f524c99b4bf7891a51
AlaaEldin <alaa12122000@gmail.com>
2023-10-10 16:22:18 -0700

    Add file #4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,36 @@
+#include "dog.h"
+
+/**
+ * new_dog - function for a new dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int i = 0, j = 0, k;
+	dog_t *dog;
+
+	while(name[i++])
+		;
+	while(owner[j++])
+		;
+
+	dog = malloc(sizeof(dog_t));
+	if (!dog)
+		return (NULL);
+
+	dog->name = malloc(sizeof(dog->name) * i);
+
+	for (k = 0; k < i; k++)
+		dog->name[k] = name[k];
+
+	dog->age = age;
+
+	dog->owner = malloc(sizeof(dog->owner) * j);
+	for(k = 0; k < j; k++)
+		dog->owner[k] = owner[k];
+
+	return (dog);
+}
