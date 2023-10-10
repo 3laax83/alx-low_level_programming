f8f7204feced365d481419929e0c7a42867b930a
AlaaEldin <alaa12122000@gmail.com>
2023-10-10 16:36:20 -0700

    Fix file #4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -21,15 +21,20 @@ dog_t *new_dog(char *name, float age, char *owner)
 		;
 
 	canine = malloc(sizeof(dog_t));
-	if (!canine)
+	if (canine = NULL)
 		return (NULL);
 
 	canine->name = malloc(sizeof(canine->name) * i);
+	if (canine->name == NULL)
+		return (NULL);
 	for (k = 0; k < i; k++)
 		canine->name[k] = name[k];
 
-	
+	canine->age = age;
+
 	canine->owner = malloc(sizeof(canine->owner) * j);
+	if (canine->owner == NULL)
+		return (NULL);
 	for(k = 0; k < j; k++)
 		canine->owner[k] = owner[k];
 
