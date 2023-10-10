0ecfb002ef15cc75c73a3a7c05e27346461ce7b7
AlaaEldin <alaa12122000@gmail.com>
2023-10-10 12:38:18 -0700

    Fix file #3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef _3_FUNCTION_LIKE_MACRO
-#define _3_FUNCTION_LIKE_MACRO
+#ifndef _3_FUNCTION_LIKE_MACRO_
+#define _3_FUNCTION_LIKE_MACRO_
 
-#define ABS(x) ((x) < 0 ? (-x) : (x))
+#define ABS(x) ((x) >= 0 ? (x) : (-x))
 
 #endif
