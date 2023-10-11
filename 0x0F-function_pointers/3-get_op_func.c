8fbc2d77277ca6d8fc7f209c35241323dde86cec
AlaaEldin <alaa12122000@gmail.com>
2023-10-11 13:39:59 -0700

    Add & Fix files

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -2,21 +2,22 @@
 
 int (*get_op_func(char *s))(int, int)
 {
-	int i = 0;
+	op_t ops[] = {
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    };
+    int i = 0;
 
-	op ops[] = 
-	{
-		{'+', op_add},
-		{'-', op_sub},
-		{'*', op_mul},
-		{'/', op_div},
-		{'%', op_mod},
-		{NULL, NULL}
-	};
+    while (i < 5)
+    {
+	    if (strcmp(s, ops[i].op) == 0)
+		    return (ops[i].f);
+	    i++;
+    }
 
-	while(ops[i].op && s[1] == 0)
-		if (ops[i].op[0] == *s)
-			return ((ops[i].f));
-
-	return (NULL);
+    return (NULL);
 }
