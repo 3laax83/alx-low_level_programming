36ffc6b9a4a501d83ffca523ed0be61b9372d654
AlaaEldin <alaa12122000@gmail.com>
2023-10-11 14:28:27 -0700

    Add & Fix files

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,6 +10,7 @@
 int main(int argc, char *argv[])
 {
 	int num1, num2;
+	int (*opr)(int, int);
 
 	if (argc != 4)
 	{
@@ -20,13 +21,13 @@ int main(int argc, char *argv[])
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 
-	if (num2 == 0 && (argv[2][0] == '/' || argv[2][0] == '%' || argv[2][1] == '/' || argv[2][1] == '%'))
+	if (num2 == 0 && (strcmp("%", argv[2]) == 0 || strcmp("/", argv[2]) == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
-
-	 int (*opr)(int, int) = get_op_func(argv[2]);
+	
+	opr = get_op_func(argv[2]);
 
 	if (opr == NULL)
 	{
