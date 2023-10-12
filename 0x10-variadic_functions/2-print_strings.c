af70d4c9904c5421bae57f43df9ee43228c81875
AlaaEldin <alaa12122000@gmail.com>
2023-10-12 10:39:52 -0700

    Add & Fix files

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings -  a function that prints strings
+ * @separator: pointer to separator included
+ * @n: number of strings passed to the function
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	int i = (int16_t) n;
+	va_list ptr;
+
+	va_start(ptr, n);
+
+	for (; i > 0; i--)
+	{
+		printf("%s", va_arg(ptr, char *));
+		if (i != 1 && separator)
+			printf("%s", separator);
+	}
+
+	va_end(ptr);
+	
+	printf("\n");
+}
