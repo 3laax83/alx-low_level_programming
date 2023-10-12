075ffbb1933ada0ed53f2cd30d552c0ac22722de
AlaaEldin <alaa12122000@gmail.com>
2023-10-12 10:44:19 -0700

    Add & Fix files

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -15,10 +15,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (; i > 0; i--)
 	{
-		if (va_arg(ptr, char *) != NULL)
-			printf("%s", va_arg(ptr, char *));
-		else
+		if (va_arg(ptr, char *) == NULL)
 			printf("(nil)");
+		else
+			printf("%s", va_arg(ptr, char *));
 		if (i != 1 && separator)
 			printf("%s", separator);
 	}
