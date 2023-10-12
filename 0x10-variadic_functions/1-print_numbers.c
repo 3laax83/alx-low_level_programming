fbcc92fd5567d260020f46eabf5e31bb32218198
AlaaEldin <alaa12122000@gmail.com>
2023-10-12 09:05:53 -0700

    Add & Fix files

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,20 +1,6 @@
 #include "variadic_functions.h"
 
 /**
- * prnt - print seperator
- * @str: pointer to string
- */
-
-void prnt(char *str)
-{
-	int i = 0;
-
-	for (; str[i] != 0; i++)
-		_putchar(str[i]);
-}
-
-
-/**
  * print_numbers - a function that prints numbers
  * @separator: pointer to the separator included
  * @n: number of numbers
@@ -28,7 +14,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(ptr, n);
 
 	for(;i > 0; i--)
-		prnt(va_arg(ptr, int));
+	{
+		printf("%d", va_arg(ptr, int));
+		if (i != 1)
+			printf("%s", separator);
+		else
+			printf("\n");
+	}
 
 	va_end(ptr);
 }
