32e20bf0036bdacda65e3604f5dbd094310f60e9
AlaaEldin <alaa12122000@gmail.com>
2023-10-19 13:24:52 -0700

    Adds & Fixes

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * free_list -  a function that frees a list_t list.
+ * @head: pointer to list_t struct
+ */
+
+void free_list(list_t *head)
+{
+	list_s *tmp;
+
+	for (tmp = head; tmp; tmp = tmp->next)
+		free(tmp);
+}
