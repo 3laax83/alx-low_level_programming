d86d63f77afc1912920eef7e5f23bb677f86a4ee
AlaaEldin <alaa12122000@gmail.com>
2023-10-24 12:10:29 -0700

    Adds & Fixes

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_listint - a function that frees a listint_t list.
+ * @head: pointer to head
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *reader;
+
+	for (; head; reader = head->next)
+	{
+		free(head);
+		head = reader;
+	}
+}
