d1f73b7e377924b9f3e41218c68735ddc07c2a28
AlaaEldin <alaa12122000@gmail.com>
2023-10-24 15:43:29 -0700

    Adds & Fixes

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,6 +1,33 @@
 #include "lists.h"
 
 /**
+ * find_listint_loop - a function that finds the loop in a linked list.
+ * @head: pointer to head
+ * Return: The address of the node where the loop starts, or NULL if there is
+ * no loop
+ */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *p, *last;
+
+	if (head == NULL)
+		return (NULL);
+
+	for (last = head->next; last; last = last->next)
+	{
+		if (last == last->next)
+			return (last);
+
+		for (p = head; p != last; p = p->next)
+			if (p == last->next)
+				return (last->next);
+	}
+
+	return (NULL);
+}
+
+/**
  * print_listint_safe -  a function that prints a listint_t linked list.
  * @head: pointer to head
  * Return: the number of nodes in the list
