21fd1303389cbe6a846b0136408351d9c162a57a
AlaaEldin <alaa12122000@gmail.com>
2023-10-24 15:57:20 -0700

    Adds & Fixes

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -9,14 +9,17 @@
 size_t free_listint_safe(listint_t **h)
 {
 	listint_t *next, *node;
-	size_t length = 0, loop = 1;
+	size_t length = 0;
+	int loop = 1;
 
 	if (*h == NULL)
 		return (0);
 
 	node = find_listint_loop(*h);
-	for (; *h && (*h != node || loop); *h = next, ++length, next = (*h)->next)
+	for (; *h && (*h != node || loop); *h = next)
 	{
+		length++;
+		next = (*h)->next;
 		if (*h == node && loop)
 		{
 			if (node == node->next)
