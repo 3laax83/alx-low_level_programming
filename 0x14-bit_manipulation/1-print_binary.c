646f131e40445fea95ba7dc9b0112f7e6c849755
AlaaEldin <alaa12122000@gmail.com>
2023-10-26 12:52:13 -0700

    Adds & Fixes

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,10 +9,10 @@ void print_binary(unsigned long int n)
 {
 	double mod;
 
-	mod = (double) (n * 0.5) - (int) (n * 0.5);
 	if (n > 1)
 		print_binary(n * 0.5);
-	if (mod == 0)
+	mod = (double) (n * 0.5) - (int) (n * 0.5);
+	if (mod == 0.0)
 		_putchar('0');
 	else
 		_putchar('1');
