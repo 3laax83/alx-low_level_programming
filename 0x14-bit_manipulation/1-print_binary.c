4435ef48175bc8816864f50b239012037335939e
AlaaEldin <alaa12122000@gmail.com>
2023-10-26 13:14:04 -0700

    Adds & Fixes

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,10 +7,11 @@
 
 void print_binary(unsigned long int n)
 {
-	if (n > 1)
-		print_binary(n >> 1);
-	if ((double) (n * 0.5) == (int) (n * 0.5))
-		_putchar('0');
-	else
-		_putchar('1');
+	if (n == 0 || n == 1)
+	{
+		_putchar(n + '0');
+		return;
+	}
+	print_binary(n >> 1);
+	_putchar('0' + (n & 1));
 }
