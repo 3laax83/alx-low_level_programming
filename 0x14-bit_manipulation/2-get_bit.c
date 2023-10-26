2f8ef9fb7639db59445eac281a207f5b637d293b
AlaaEldin <alaa12122000@gmail.com>
2023-10-26 13:28:49 -0700

    Adds & Fixes

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,9 +9,12 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
+	unsigned long int i;
+
 	if (index > 8 * sizeof(unsigned long int) - 1)
 		return (-1);
-	if (1 << index == n & (1 << index))
+	i = 1 << index;
+	if (i  == (n & i))
 		return (1);
 	return (0);
 }
