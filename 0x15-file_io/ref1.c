1ba4faedd62116def3378fa85381d58d3a3bf4de
AlaaEldin <alaa12122000@gmail.com>
2023-10-30 23:57:29 +0000

    small fix

--- 0x15-file_io/ref1.c
+++ 0x15-file_io/ref1.c
@@ -134,7 +134,7 @@ void printOSABI(unsigned char *e_ident)
 
 void printAbi(unsigned char *e_ident)
 {
-	printf("  ABI Version:                       %d",
+	printf("  ABI Version:                       %d\n",
 			e_ident[EI_ABIVERSION]);
 }
 
