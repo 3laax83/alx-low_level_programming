e2f0d265799222f7a0e5cecab4bb40b2d48d113e
AlaaEldin <alaa12122000@gmail.com>
2023-10-30 13:27:20 -0700

    Adds & Fixes

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,7 +11,7 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int file, num, rw;
 
-	if(!filename)
+	if (!filename)
 		return (-1);
 
 	file = open(filename, O_WRONLY | O_APPEND);
