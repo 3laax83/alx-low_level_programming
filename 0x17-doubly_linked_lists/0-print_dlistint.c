40cc0fdc7cd58c37cb8739dadd39ab7f43d55373
AlaEldin <aeiedamo@gmail.com>
2023-12-07 19:19:58 +0300

    Fix #0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,15 +7,15 @@
 */
 size_t print_dlistint(const dlistint_t *h)
 {
-    int count = 0;
-    if (!h)
-        return (0);
-    
-    do
-    {
-        count++;
-        printf("%d\n", h->n);
-    } while (h && (h = h->next));
-    
-    return (count);
+	int count = 0;
+
+	if (!h)
+		return (0);
+
+	do {
+		count++;
+		printf("%d\n", h->n);
+	} while (h && (h = h->next));
+
+	return (count);
 }
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -11,13 +11,13 @@
  * @next: points to the next node
  *
  * Description: doubly linked list node structure
- * 
+ *
  */
 typedef struct dlistint_t
 {
-    int n;
-    struct dlistint_t *prev;
-    struct dlistint_t *next;
+	int n;
+	struct dlistint_t *prev;
+	struct dlistint_t *next;
 } dlistint_t;
 size_t print_dlistint(const dlistint_t *h);
 #endif /*LISTS_H*/
