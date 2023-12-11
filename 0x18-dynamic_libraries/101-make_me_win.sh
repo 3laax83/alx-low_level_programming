81a87511952bb356901509c42ca6b33b8907fb89
AlaEldin <aeiedamo@gmail.com>
2023-12-11 19:59:29 +0300

    Still testing #3...

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cp ./neogm.so /tmp/neogm.so
+wget -P /tmp https://github.com/aeiedamo/alx-low_level_programming/blob/main/0x18-dynamic_libraries/neogm.so
 export LD_PRELOAD=/tmp/neogm.so
