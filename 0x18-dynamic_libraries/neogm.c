745dfdc6a7b189cdd9cfec1bb71bcd6268a092c2
AlaEldin <aeiedamo@gmail.com>
2023-12-11 20:01:41 +0300

    testing #3

--- 0x18-dynamic_libraries/neogm.c
+++ 0x18-dynamic_libraries/neogm.c
@@ -3,23 +3,22 @@
 
 int neogm()
 {
-        static int gm = -1;
+        static int gm = 0;
 
-        gm++;
         switch (gm)
         {
                 case 0:
-                        return (8);
-                case 1:
-                        return (8);
-                case 2:
-                        return (7);
-                case 3:
                         return (9);
+                case 1:
+                        return (9);
+                case 2:
+                        return (8);
+                case 3:
+                        return (10);
                 case 4:
-                        return (23);
+                        return (24);
                 case 5:
-                        return (74);
+                        return (75);
                 default:
                         return (gm * gm % 30000);
         }
