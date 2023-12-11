4503db89fab507e6eba8d594ea0bb9639d804af1
AlaEldin <aeiedamo@gmail.com>
2023-12-11 20:50:14 +0300

    Testing #3

--- 0x18-dynamic_libraries/neogm.c
+++ 0x18-dynamic_libraries/neogm.c
@@ -1,22 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 
 int rand()
 {
-	static int ct = -1;
+        static int gm = -1;
 
-	ct++;
-	if (ct == 0)
-		return 8;
-	if (ct == 1)
-		return 8;
-	if (ct == 2)
-		return 7;
-	if (ct == 3)
-		return 9;
-	if (ct == 4)
-		return 23;
-	if (ct == 5)
-		return 74;
-	return ct;
+        ++gm;
+        switch (gm)
+        {
+                case 0:
+                case 1:
+                        return (8);
+                case 2:
+                        return (7);
+                case 3:
+                        return (9);
+                case 4:
+                        return (23);
+                case 5:
+                        return (74);
+                default:
+                        return (gm);
+        }
 }
