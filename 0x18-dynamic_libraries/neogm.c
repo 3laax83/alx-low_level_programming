47a61dd9b4fc3ed6e29aaebe4c7a35d1e6fd1f15
AlaEldin <aeiedamo@gmail.com>
2023-12-11 20:10:20 +0300

    still testing #3

--- 0x18-dynamic_libraries/neogm.c
+++ 0x18-dynamic_libraries/neogm.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-int neogm()
+int rand()
 {
         static int gm = 0;
 
@@ -10,16 +11,16 @@ int neogm()
                 case 0:
                         return (9);
                 case 1:
-                        return (9);
-                case 2:
                         return (8);
-                case 3:
+                case 2:
                         return (10);
-                case 4:
+                case 3:
                         return (24);
-                case 5:
+                case 4:
                         return (75);
+                case 5:
+                        return (9);
                 default:
-                        return (gm * gm % 30000);
+                        return (gm);
         }
 }
