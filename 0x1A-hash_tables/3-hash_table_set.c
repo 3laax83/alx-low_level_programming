fb6cddc389c98f2c9023a79503b228241a104128
AlaEldin <aeiedamo@gmail.com>
2023-12-22 23:29:18 +0300

    a test for #3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,37 +1,100 @@
 #include "hash_tables.h"
-#include "node_create_update.c"
 
 /**
- * hash_table_set - function to set the key, value in a table
- * @ht: pointer to hash table
- * @key: the key to be set
- * @value: the value to be set
- * Return: 1 if succeed, 0 otherwise
-*/
-
+ * hash_table_set - insert a key, value pair to a hash table.
+ * @ht: hash table to insert to.
+ * @key: key
+ * @value: value to be stored at that key.
+ *
+ * Return: 1 on SUCCESS otherwise 0
+ */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned int i;
-	char *str = NULL;
+	unsigned long int idx;
+	char *str;
 	hash_node_t *node;
 
 	if (!ht || !key)
 		return (0);
-	i = key_index((unsigned char *)key, ht->size);
+
+	idx = key_index((unsigned char *)key, ht->size);
 	if (value)
 		str = strdup(value);
+	else
+		str = NULL;
 
-	node = ht->array[i];
+	node = ht->array[idx];
+
 	if (!node)
-		node = node_create(NULL, key, str);
+		node = new_node(NULL);
+
 	if (node->key)
 	{
-		ht->array[i] = node_update(node, key, str);
+		ht->array[idx] = update_node(node, key, str);
 		return (1);
 	}
 	node->key = strdup(key);
 	node->value = str;
 	node->next = NULL;
-	ht->array[i] = node;
+
+	ht->array[idx] = node;
 	return (1);
+}
+
+/**
+ * update_node - update or added a node if one already exists
+ *
+ * @node: pointer to a node the exist
+ * @key: key
+ * @value: value to update/create node with.
+ *
+ * Return: pointer to head of a list
+ */
+hash_node_t *update_node(hash_node_t *node, const char *key, char *value)
+{
+	hash_node_t *tmp = node;
+
+	while (tmp)
+	{
+
+		if (!strcmp(key, tmp->key))
+		{
+			free(tmp->value);
+			tmp->value = value;
+			return (node);
+		}
+		tmp = tmp->next;
+	}
+
+	tmp = node;
+	node = new_node(node);
+	if (!node)
+	{
+		node = tmp;
+		return (node);
+	}
+	node->key = strdup(key);
+	node->value = value;
+	return (node);
+
+}
+
+/**
+ * new_node - create a new node
+ * @c_node: current node to add add at the end.
+ *
+ * Return: a point to the new node.
+ */
+hash_node_t *new_node(hash_node_t *c_node)
+{
+	hash_node_t *node;
+
+	node = malloc(sizeof(hash_node_t));
+	if (!node)
+		return (NULL);
+	node->key = NULL;
+	node->value = NULL;
+	node->next = c_node;
+
+	return (node);
 }
