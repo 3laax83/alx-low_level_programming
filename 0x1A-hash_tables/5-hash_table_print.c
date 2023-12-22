ca5b16f65540bc0d3be4456ef311876cf3bb7421
AlaEldin <aeiedamo@gmail.com>
2023-12-23 00:34:03 +0300

    Add solution for #5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+void hash_table_print(const hash_table_t *ht)
+{
+    unsigned int i, sign;
+    hash_node_t *tmp;
+
+    if (!ht)
+        return;
+    printf("{");
+    for (i = 0, sign = 0; i < ht->size; i++)
+    {
+        tmp = ht->array[i];
+        for (; tmp; tmp = tmp->next)
+        {
+            printf("\'%s\': \'%s\'", tmp->key, tmp->value);
+
+            if (tmp->next)
+                printf(", ");
+        }
+        for (; !(ht->array[i+1]); i++)
+            ;
+        if (sign && i < ht->size)
+            printf(", ");
+        else
+            sign = 1;
+    }
+    printf("}\n");
+}
