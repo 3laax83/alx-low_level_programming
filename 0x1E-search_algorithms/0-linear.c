7cfd13a33b03e63211c40c8e833f83b16bf25db5
AlaEldin Mohamed <aeiedamo@gmail.com>
2024-05-10 17:34:00 +0100

    0x1E - fixes for #0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -6,19 +6,20 @@
  * @array: pointer to array
  * @size: size of array
  * @value: value to search for
+ * Return: index of value if found, -1 otherwise
  */
 
-int linear_search(int *array, size_t size, int value) {
-  size_t i;
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
 
-  if (!array)
-	return (-1);
-
-  for (i = 0; i < size; i++) {
-	printf("Value checked array[%lu] = [%d]\n", i, *(array + i));
-	if (*(array + i) == value) {
-	  return (i);
+	if (!array || size < 1)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, *(array + i));
+		if (*(array + i) == value)
+			return (i);
 	}
-  }
-  return (-1);
+	return (-1);
 }
