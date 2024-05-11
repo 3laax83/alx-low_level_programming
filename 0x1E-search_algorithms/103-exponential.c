0e1d369b089f496b7aa77a51d5ed74e46f485ec9
AlaEldin Mohamed <aeiedamo@gmail.com>
2024-05-11 19:17:20 +0100

    0x1E - final fix for #103

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -20,9 +20,11 @@ int exponential_search(int *array, size_t size, int value)
 		for (i = 1; i < size && array[i] <= value; i *= 2)
 			printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 	if (i < size)
-		r= i;
+		r = i;
 	else
-		r= size - 1;
+		r = size - 1;
 
-	return (i / 2 + binary_search(array + i / 2, r- i / 2 + 1, value));
+	printf("Value found between indexes [%lu] and [%lu]\n", i / 2, r);
+
+	return (i / 2 + binary_search(array + i / 2, r - i / 2 + 1, value));
 }
